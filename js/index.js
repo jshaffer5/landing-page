aa32bb4cf72ba79daf76cdcc567e6df4fb411eaa
js <joshtwoone@gmail.com>
2019-01-29 02:54:35 -0800

    final bug fix for service card box-shadows

--- js/index.js
+++ js/index.js
@@ -11,34 +11,16 @@ const myFunction = () => {
   }
 }
 
-function showMore() {
-  let htmlCollection = document.getElementsByClassName("more-cards");
-  let elements = Array.from(htmlCollection);
-  console.log("showMore() called");
-  elements.map(element => {
-    if (element.style.display != "block") {
-      element.style.display = "block";
-      document.getElementById("show-more-projects").innerText = "Show Less";
-    } else {
-      element.style.display = "none";
-      document.getElementById("show-more-projects").innerText = "Show More";
-    }
-  });
-}
-
 window.onload = function() {
   $(".service-card").fadeIn("fast", function() {
-    let htmlCollection = document.getElementsByClassName("service-card");
-    let elements = Array.from(htmlCollection);
-    elements.map(element => {
-      $(element).hover(function() {
-        $(this).css("box-shadow", "0 8px 16px 0 rgba(0, 0, 0, 0.2)");
-        }, 
-        function() {
-          $(this).css("box-shadow", "0 4px 8px 0 rgba(0, 0, 0, 0.2)");
-        }
-      );
-      
-    });
+    // place a shadow on the card
+    $(this).css("box-shadow", "0 4px 8px 0 rgba(0, 0, 0, 0.2)");
+    $(this).hover(function() { // enhance shadow on hover
+      $(this).css("box-shadow", "0 8px 16px 0 rgba(0, 0, 0, 0.2)");
+      }, 
+      function() { // return to default shadow after hover ends
+        $(this).css("box-shadow", "0 4px 8px 0 rgba(0, 0, 0, 0.2)");
+      }
+    );
   });
 };
